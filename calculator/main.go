77a441ff8ca52d4d31d5d855fae39128a0132071
David Polania <davidpolaniaac@users.noreply.github.com>
2022-06-09 22:43:17 -0500

    fix calculator v1

--- calculator/main.go
+++ calculator/main.go
@@ -9,18 +9,51 @@ import (
 	"strings"
 )
 
-func main() {
+type Calc struct{}
+
+func parse(value string) int {
+	data, err := strconv.Atoi(value)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return data
+}
+
+func (Calc) operate(input string, operator string) int {
+	values := strings.Split(input, operator)
+	var result int
+	for index, value := range values {
+		data := parse(value)
+		if index == 0 {
+			result = data
+		} else {
+			switch operator {
+			case "+":
+				result += data
+			case "-":
+				result -= data
+			case "*":
+				result *= data
+			case "/":
+				result /= data
+			default:
+				log.Fatalln("Invalid operator", operator)
+			}
+		}
+	}
+
+	return result
+}
+
+func (Calc) scanner() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	operation := scanner.Text()
-	values := strings.Split(operation, "+")
-	var result int = 0
-	for _, value := range values {
-		data, err := strconv.Atoi(value)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		result += data
-	}
-	fmt.Println("Total:", result)
+	return scanner.Text()
+}
+
+func main() {
+	var calc Calc
+	operation := calc.scanner()
+	result := calc.operate(operation, "+")
+	fmt.Println(operation, "=", result)
 }
